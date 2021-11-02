0fa10ce73abdf3bf476dc3e887365aa00bd2086c
Donard97 <donardgolaj@gmail.com>
2021-11-02 01:57:42 +0100

    Write queries for animals

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT name FROM animals WHERE name LIKE '%mon';
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
+
+SELECT name FROM animals WHERE neutered IS TRUE AND escape_attempts<3;
+
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
+
+SELECT name FROM animals WHERE neutered IS TRUE;
+
+SELECT name FROM animals WHERE name NOT IN ('Gabumon');
+
+SELECT name FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
