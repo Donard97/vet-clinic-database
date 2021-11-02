03c4ef30d5410bb34c702a9d256bb4807e4298c9
Donard97 <donardgolaj@gmail.com>
2021-11-02 14:17:18 +0100

    Delete transactions file and put transactions to queries file

--- queries.sql
+++ queries.sql
@@ -9,6 +9,29 @@ SELECT name FROM animals WHERE neutered IS TRUE;
 SELECT name FROM animals WHERE name NOT IN ('Gabumon');
 SELECT name FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+SAVEPOINT dateOfBirth;
+UPDATE animals SET weight_kg=-1*weight_kg;
+ROLLBACK TO dateOfBirth;
+UPDATE animals SET weight_kg=-1*weight_kg WHERE weight_kg<0;
+COMMIT; 
+
+
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
 SELECT AVG(weight_kg) FROM animals;
