e06d2c2d7c18128e2d570a4287f1b7f75665a46c
Donard97 <donardgolaj@gmail.com>
2021-11-08 16:06:56 +0100

    Add extra column to the owners table

--- schema.sql
+++ schema.sql
@@ -60,3 +60,5 @@ CREATE TABLE visits (
   CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
   CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
 );
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
